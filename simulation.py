98e410ede0eaff3184e190473bee78350e896353
Chris Michael Jones <cjones1688@gmail.com>
2020-03-04 07:20:56 -0500

    added psuedo code start for functions

---
+++ simulation.py
@@ -0,0 +1,67 @@
+
+# PoPS Pandemic - simulation
+#
+# Copyright (C) 2020-2020 by the authors.
+#
+# Authors: Chris Jones (cmjone25 ncsu edu)
+#
+# The code contained herein is licensed under the GNU General Public
+# License. You may obtain a copy of the GNU General Public License
+# Version 3 or later at the following locations:
+#
+# http://www.opensource.org/licenses/gpl-license.html
+# http://www.gnu.org/copyleft/gpl.html
+
+
+# Read in Data
+
+# Calculate probability of introduction for each pest/pathogen between countries (including probability of re-introduction?)
+    # trade 
+    # species presence
+    ## phytosanitary compliance origin
+    ## phytosanitary compliance arrival
+    ## commodity type
+    ### host similarity between countries
+    ### seasonality of host country and species lifecycle
+
+# Calculate probability of establishment for each pest/pathogen between countries (maybe a categorical prediction)
+    # Seasonality
+    # Environmental similarity
+    ## host similarity
+
+
+## function list
+# trade
+# presence (pest)
+# phytosanitary compliance
+# climate match
+# host similirity
+# Seasonality
+# 
+
+
+# Trade function
+    #1 total volume
+    #2 commodity type (ag, forest, manufactured, waste)
+    #3 commodity
+
+
+
+
+
+
+
+
+def host_similarity(source, country_introduction):
+    ## this is a function to determine the similarity between country of origin and country of introduction
+    print(s)
+
+def introduction(s):
+    print(s)
+
+
+def establishment(s):
+    print(s)
+
+def invasion(s):
+    print(s)
