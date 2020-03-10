07c443c3185c7a12366376cddc6240b68561d30d
Chris Michael Jones <cjones1688@gmail.com>
2020-03-10 16:37:58 -0400

    created baseline pandemic simulation

--- model.py
+++ model.py
@@ -1,10 +1,38 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
 # This runs the loop across the functions in the simulation
 
-locations = ["United States", "China"]
-trade = [[0, 50], [50, 0]]
+def arrival(arrivals, trades, distances, locations, index):
+    for ind in range(len(locations)):
+        location = locations.iloc[index,:]
+        #print(location['phytosanitary_compliance'])
+        #print(trades[index,ind])
+        #print(type(distances))
+        arrivals[index, ind] = trades[index, ind] * distances[index, ind] #* location['phytosanitary_compliance'] # * locations.establishment
+    return(arrivals)
 
-def invasion(locations):
-    for location in locations:
-        print(location)
+#locations = pd.DataFrame({'name': ["United States", "China", "Brazil"], 'phytosanitary_compliance': [0.25, 0.50, 0.75], 'Presence': [True, False, True]})
+#trades = np.array([[0, 50, 15], [50, 0, 10], [20, 30, 0]])
 
-invasion(locations)
+#distances = np.array([[1, 5000, 105000], [5000, 1, 7500], [10500, 7500, 1]])
+
+#distances = 1/distances
+
+#trades = np.genfromtxt("Data/TradeData.csv", delimiter=',', names= True, dtype=int, usecols=range(0,242))
+trades = pd.read_csv("Data/TradeData.csv")
+trades = trades.iloc[0:241,1:242].to_numpy()
+locations = pd.read_csv("Data/Presence.csv")
+arrivals = np.empty(shape=(len(trades),len(trades)), dtype='object')
+distances = np.ones_like(arrivals)
+#locations = locations.iloc[0:241,]
+
+def invasion(arrivals, trades, distances, locations):
+    for ind in range(len(locations)):
+        location = locations.iloc[ind,:]
+        if (location['Presence']):
+            arrivals = arrival(arrivals, trades, distances, locations, ind)
+    return(arrivals)
+
+invasion(arrivals, trades, distances, locations)
+print(arrivals)
