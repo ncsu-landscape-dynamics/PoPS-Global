d517fe7b318460c1fabe08b0b72c12fd00e690f6
ChrisJones687 <cjones1688@gmail.com>
2020-05-06 11:21:35 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="pandemic",
+    version="0.0.1",
+    author="Chris Jones",
+    author_email="cmjone25@ncsu.edu",
+    description="Simulation for PoPS Pandemic Model",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/ncsu-landscape-dynamics/Pandemic_Model",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires=">=3.5",
+    install_requires=["numpy", "scipy", "PyYAML", "math", "pandas"],
+)
