a0a9176609070ec43b8ce63d63d2eceb4062b64b
ChrisJones687 <cjones1688@gmail.com>
2020-05-06 06:45:39 -0400

    added climate similarity matching

---
+++ pandemic/ecological_calculations.py
@@ -0,0 +1,48 @@
+
+"""
+PoPS Pandemic - Simulation
+
+Module containing all calcualtions for ecological similarity
+
+Copyright (C) 2019-2020 by the authors.
+
+Authors: Chris Jones (cmjone25 ncsu edu)
+
+The code contained herein is licensed under the GNU General Public
+License. You may obtain a copy of the GNU General Public License
+Version 3 or later at the following locations:
+
+http://www.opensource.org/licenses/gpl-license.html
+http://www.gnu.org/copyleft/gpl.html
+"""
+
+
+def climate_similarity(origin_climates, destination_climates):
+    """
+    Returns the climate similarity between origin (i) and destion (j) by
+    simply checking whether or not the climate type is present in both the
+    origin (i) and destination (j) and summing the total area in the
+    destination (j) that is also in the origin (i).
+
+    Parameters
+    ----------
+    origin_climates : array (float)
+        An array with percent area for each of the Koppen climate zones for the
+        origin (i)
+    destination_climates : array (float)
+        An array with percent area for each of the Koppen climate zones for the
+        destination (j)
+
+    Returns
+    -------
+    probability_of_entry : float
+        The probability of a pest to enter the origin location
+
+    """
+
+    similarity = 0.00
+    for clim in range(len(origin_climates)):
+        if (origin_climates[clim] > 0) and destination_climates[clim] > 0:
+            similarity += destination_climates[clim]
+
+    return similarity
