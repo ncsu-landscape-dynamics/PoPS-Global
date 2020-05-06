5d20c971b074e3d890cee94d140f63ea9f699e1f
ChrisJones687 <cjones1688@gmail.com>
2020-05-06 15:04:07 -0400

    added basic tests for probabilities

---
+++ tests/test_probability_calculations.py
@@ -0,0 +1,103 @@
+import pytest
+import numpy as np
+from pandemic.probability_calculations import (
+    probability_of_entry,
+    probability_of_establishment,
+    probability_of_introduction,
+)
+
+
+def test_probability_of_entry():
+
+    assert (
+        probability_of_entry(
+            rho_i=1,
+            rho_j=0,
+            zeta_it=1,
+            lamda_c=1,
+            T_ijct=500,
+            sigma_T=200,
+            mu=0.02,
+            d_ij=150,
+            chi_it=1,
+        )
+        == 0
+    )
+
+    assert (
+        probability_of_entry(
+            rho_i=0,
+            rho_j=1,
+            zeta_it=1,
+            lamda_c=1,
+            T_ijct=500,
+            sigma_T=200,
+            mu=0.02,
+            d_ij=150,
+            chi_it=1,
+        )
+        == 0
+    )
+
+    assert (
+        probability_of_entry(
+            rho_i=0,
+            rho_j=0,
+            zeta_it=0,
+            lamda_c=1,
+            T_ijct=500,
+            sigma_T=200,
+            mu=0.02,
+            d_ij=150,
+            chi_it=1,
+        )
+        == 0
+    )
+
+    assert (
+        probability_of_entry(
+            rho_i=0,
+            rho_j=0,
+            zeta_it=1,
+            lamda_c=1,
+            T_ijct=500,
+            sigma_T=200,
+            mu=0.02,
+            d_ij=150,
+            chi_it=0,
+        )
+        == 0
+    )
+
+    # assert (
+    #     probability_of_entry(
+    #         rho_i=0,
+    #         rho_j=0,
+    #         zeta_it=1,
+    #         lamda_c=1,
+    #         T_ijct=200,
+    #         sigma_T=200,
+    #         mu=0.0002,
+    #         d_ij=2000,
+    #         chi_it=1,
+    #     )
+    #     == 0
+    # )
+
+
+def test_probability_of_establishment():
+    assert (
+        probability_of_establishment(
+            alpha=0,
+            beta=1,
+            delta_kappa_ijt=1,
+            sigma_kappa=1,
+            h_jt=1,
+            sigma_h=1,
+            epsilon_jt=1,
+            sigma_epsilon=1,
+            phi=1,
+            sigma_phi=2,
+        )
+        == 0
+    )
