a6a07ba9ecb2eb246a50379559da637b3b32a6f0
Kellyn Paige Montgomery <kpmontgo@wolftech.ad.ncsu.edu>
2020-06-26 11:58:01 -0400

    Add monthly option. Saves matrix per month.

--- Data/Comtrade/get_comtrade.py
+++ Data/Comtrade/get_comtrade.py
@@ -65,82 +65,75 @@ def nested_list(original_list, list_length):
 auth_code = "jXIKwJ2httdcPDHwwJCj7GzbDh8fva23HYV17lyN+BeKrxX3fSviSAT9vgH5zQ+XnKj75SBnqPn25kXrwD1viUgtdDMNhpjrw4ZPcpdznaYq1nH8F/wxSoUBSMUzwVVb3YsoqruN04qDiJU/NleTCA=="
 
 # add lines to import HS list and loop through codes of interest at 4 digits
-# hs = '2501'
+# hs = '6801'
 
-# hs_list = np.arange(2501, 2530 + 1, 1)
-# hs_code_str = "%2C".join(map(str, hs_list))
+freq = "M" # Set time step, options are A (annual) or M (monthly)
+start_year = 2000
+end_year = 2018
+years = np.arange(start_year, end_year + 1, 1)
 
-# start_year = 2000
-# end_year = 2018
-# years = np.arange(start_year, end_year + 1, 1)
-# years_str = "%2C".join(map(str, years))
-
-# country_code_list = list(country_code['id'])
-# country_code_str = "%2C".join(country_code_list)
-
-# url = urlopen('http://comtrade.un.org/api/get/plus?max=250000&type=C&px=HS&cc=' + hs + '&r=' + country_code_str + '&rg=1&p=all&freq=A&ps=' + years_str + '&fmt=json&token=' + str(auth_code))
-# raw = json.loads(url.read().decode())
-# url.close()
-
+if freq == "M":
+    timesteps = []
+    for year in years:
+        months = ['01', '02', '03', '04', '05','06', '07', '08', '09', '10', '11', '12']
+        for month in months:
+            timesteps.append(str(year) + month)
+    else:
+        timesteps = years
 
 hs_list = np.arange(6801, 6815 + 1, 1)
 for hs in hs_list:
-    start_year = 2000
-    end_year = 2018
-    years = np.arange(start_year, end_year + 1, 1)
-    nested_years = nested_list(years, 5)
     nested_country_codes = nested_list(list(country_code['id']), 5)
-    for year_list in nested_years:
-        years_str = "%2C".join(map(str, year_list))
-        data = pd.DataFrame()
-        for country_code_list in nested_country_codes:  # loop over all countries
-            # time.sleep(45)  # prob not needed
-            country_code_str = "%2C".join(country_code_list)
+    years_str = "%2C".join(map(str, years))
+    data = pd.DataFrame()
+    for country_code_list in nested_country_codes:  # loop over all countries
+        # time.sleep(45)  # prob not needed
+        country_code_str = "%2C".join(country_code_list)
+        try:
+            url = urlopen('http://comtrade.un.org/api/get?max=250000&type=C&px=HS&cc=' + str(hs) + '&r=' + country_code_str + '&rg=1&p=all&freq=' + freq + '&ps=' + years_str + '&fmt=json&token=' + str(auth_code))
+            raw = json.loads(url.read().decode())
+            url.close()
+        except:  # if did not load, try again
             try:
-                url = urlopen('http://comtrade.un.org/api/get?max=250000&type=C&px=HS&cc=' + str(hs) + '&r=' + country_code_str + '&rg=1&p=all&freq=A&ps=' + years_str + '&fmt=json&token=' + str(auth_code))
+                url = urlopen('http://comtrade.un.org/api/get?max=250000&type=C&px=HS&cc=' + str(hs) + '&r=' + country_code_str + '&rg=1&p=all&freq=' + freq + '&ps=' + years_str + '&fmt=json&token=' + str(auth_code))
                 raw = json.loads(url.read().decode())
                 url.close()
-            except:  # if did not load, try again
-                try:
-                    url = urlopen('http://comtrade.un.org/api/get?max=250000&type=C&px=HS&cc=' + str(hs) + '&r=' + country_code_str + '&rg=1&p=all&freq=A&ps=' + years_str + '&fmt=json&token=' + str(auth_code))
-                    raw = json.loads(url.read().decode())
-                    url.close()
-                except:  # if did not load again, move on to the next country in the loop
-                    error_log.writerow(['', country_code_str, hs, years_str, 'Fail', '', time.ctime()])
-                    print('Fail: country ' + country_code_str + ', ' + years_str + ", " + str(hs))
-                    continue
-
-            if len(raw['dataset']) == 0:
-                print('No data downloaded for:' + years_str + ' ' + country_code_str + '. Message: ' + str(raw['validation']['message']))
+            except:  # if did not load again, move on to the next country in the loop
+                error_log.writerow(['', country_code_str, hs, years_str, 'Fail', '', time.ctime()])
+                print('Fail: country ' + country_code_str + ', ' + years_str + ", " + str(hs))
                 continue
 
-            data = data.append(raw['dataset'])          
-            data['ptCode']=data['ptCode'].astype(str)
+        if len(raw['dataset']) == 0:
+            print('No data downloaded for:' + years_str + ' ' + country_code_str + '. Message: ' + str(raw['validation']['message']))
+            continue
+
+        data = data.append(raw['dataset'])          
+        data['ptCode']=data['ptCode'].astype(str)
 
 
-        for year in year_list:
-            yr_data = data[data.yr.eq(year)]
-            HS_matrix = country_code[['id']]
-            for reporter in list(country_code['id']):
-                reporter_data = yr_data[yr_data.rtCode.eq(int(reporter))]
-                reporter_data = reporter_data[['ptCode', 'NetWeight']]
+    for timestep in timesteps:
+        timestep_data = data[data.period.eq(int(timestep))]
+        HS_matrix = country_code[['id']]
+        for reporter in list(country_code['id']):
+            reporter_data = timestep_data[timestep_data.rtCode.eq(int(reporter))]
+            reporter_data = reporter_data[['ptCode', 'NetWeight']]
 
-                # if no data was downloaded for reporter/year, add column of zeros to commodity/year df and move to next country
-                if len(reporter_data) == 0:
-                    HS_matrix = HS_matrix.assign(x = 0)
-                    HS_matrix.rename(columns={"x": reporter}, inplace=True)
-                    error_log.writerow([country_code[country_code['id'] == reporter]['text'].tolist()[0], reporter, hs, year, 'no data', raw['validation']['message'], time.ctime()])
-                    print('No data: country ' + reporter + ' ' + str(year))
-                    continue
+            # if no data was downloaded for reporter/year, add column of zeros to commodity/year df and move to next country
+            if len(reporter_data) == 0:
+                HS_matrix = HS_matrix.assign(x = 0)
+                HS_matrix.rename(columns={"x": reporter}, inplace=True)
+                error_log.writerow([country_code[country_code['id'] == reporter]['text'].tolist()[0], reporter, hs, timestep, 'no data', raw['validation']['message'], time.ctime()])
+                print('No data: country ' + reporter + ' ' + str(timestep))
+                continue
 
-                # Merge to commodity/year df
+            # Merge to commodity/year df
 
-                HS_matrix = pd.merge(HS_matrix, reporter_data, how = 'left', left_on='id', right_on='ptCode')
-                HS_matrix.drop("ptCode", axis=1, inplace=True)
-                HS_matrix.rename(columns={"NetWeight": reporter}, inplace=True)
-                print(reporter + " " + str(year) + ": finished")
+            HS_matrix = pd.merge(HS_matrix, reporter_data, how = 'left', left_on='id', right_on='ptCode')
+            HS_matrix.drop("ptCode", axis=1, inplace=True)
+            HS_matrix.rename(columns={"NetWeight": reporter}, inplace=True)
+            print(reporter + " " + str(timestep) + ": finished")
 
-            HS_matrix.fillna(0, inplace=True)
-            HS_matrix.to_csv('csv/' + str(hs) + '_' + str(year) + '.csv', index=False)
+        HS_matrix.fillna(0, inplace=True)
+        HS_matrix.to_csv('csv/' + str(hs) + '_' + str(timestep) + '.csv', index=False)
 
 csv_file.close()
