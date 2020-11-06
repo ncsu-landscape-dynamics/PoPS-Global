a4cfc318c6edc4fbbca770d098974c9f769bbd7f
kellynm <32572763+kellynm@users.noreply.github.com>
2020-11-06 10:40:29 -0500

    Add ISO3 and UN codes to phytosanitary data

---
+++ Data/add_iso3_phyto.py
@@ -0,0 +1,62 @@
+import os
+import pycountry
+import pandas
+from fuzzywuzzy import process
+
+os.chdir("H:/Shared drives/APHIS  Projects/Pandemic/Data/phytosanitary_capacity/")
+
+capacities = pandas.read_csv(
+    "Report_summary_english_spanish_french_russian_arabic_forPatricia.csv",
+    encoding="ISO-8859-1",
+)
+
+# Drop rows with no data
+capacities = capacities[capacities["Report_ava"].notna()]
+
+# Get list of ISO country names
+country_objs = list(pycountry.countries)
+names = [o.name for o in country_objs]
+
+# Use fuzzy string matching to match capacity data names with ISO names.
+capacities["iso_name"] = ""
+capacities["match_score"] = 0
+for index in capacities.index:
+    (
+        capacities.loc[index, "iso_name"],
+        capacities.loc[index, "match_score"],
+    ) = process.extractOne(capacities.loc[index, "Report_ava"], names)
+
+# List country name matches with score less than 98 for manual check.
+check = capacities.loc[
+    capacities["match_score"] < 100, ["Report_ava", "iso_name", "match_score"]
+]
+check
+
+# Create dictionary of manual corrections for mismatches.
+corrections = {
+    "Bosnia and Herz.": "Bosnia and Herzegovina",
+    "Korea (South Korea)": "Korea, Republic of",
+    "St. Vin. and Gren.": "Saint Vincent and the Grenadines",
+    "Swaziland": "Eswatini",
+}
+
+# Replace ISO name with correct value.
+for key in corrections.keys():
+    capacities.loc[capacities["Report_ava"] == key, "iso_name"] = corrections[key]
+
+
+# Add ISO3 code based on ISO names
+capacities["ISO3"] = ""
+
+for index in capacities.index:
+    capacities.loc[index, "ISO3"] = (
+        pycountry.countries.get(name=capacities.loc[index, "iso_name"])
+    ).alpha_3
+
+# Add UN codes if needed
+un_to_iso_path = "H:/Shared drives/APHIS  Projects/Pandemic/Data/Country_list_shapefile/un_to_iso.csv"
+un_to_iso = pandas.read_csv(un_to_iso_path)
+capacities = capacities.merge(un_to_iso, "left", on="ISO3")
+# capacities.UN_id = capacities.UN_id.astype(int)
+capacities.sort_values(by="UN")
+capacities.to_csv("phytosanitary_capacity_iso3.csv")
