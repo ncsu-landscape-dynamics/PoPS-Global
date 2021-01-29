01cb06bb62696e4eca8b512843777e4dc6bf61c2
kellynm <kpmontgo@ncsu.edu>
2021-01-29 15:50:34 -0500

    Use hyperbolic tan, log transform, min/max normalization

--- pandemic/probability_calculations.py
+++ pandemic/probability_calculations.py
@@ -70,7 +70,17 @@ def probability_of_entry(
         (1 - rho_i)
         * (1 - rho_j)
         * zeta_it
-        * (math.exp(lamda_c * ((T_ijct - min_Tc) / (max_Tc - min_Tc))))
+        * lamda_c
+        * (
+            1
+            - (
+                2
+                / (
+                    (math.exp(2 * ((math.log(T_ijct) - min_Tc) / (max_Tc - min_Tc))))
+                    + 1
+                )
+            )
+        )
         * math.exp((-1) * mu * d_ij)
         * chi_it
     )
