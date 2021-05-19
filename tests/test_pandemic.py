f5132a24a5bfb9bf17705a3041c8b9213e7bb746
cawalden <cawalden@ncsu.edu>
2021-05-19 15:42:55 -0500

    revised tests based on equation revisions

--- tests/test_pandemic.py
+++ tests/test_pandemic.py
@@ -7,6 +7,8 @@ from pandemic.model_equations import pandemic_single_time_step
 def test_pandemic_runs():
 
     trade = np.array([[0, 500, 15], [50, 0, 10], [20, 30, 0]])
+    min_Tc = np.min(trade)
+    max_Tc = np.max(trade)
     distances = np.array([[1, 5000, 105000], [5000, 1, 7500], [10500, 7500, 1]])
     climate_similarities = np.array([[1, 0.95, 0.4], [0.95, 1, 0.5], [0.4, 0.5, 1]])
     time_step = "2015"
@@ -39,11 +41,11 @@ def test_pandemic_runs():
         mu=0.0002,
         lamda_c=1,
         phi=5,
-        sigma_epsilon=0.5,
         sigma_h=0.5,
         sigma_kappa=0.5,
-        sigma_phi=2,
-        sigma_T=20,
+        w_phi=1,
+        min_Tc=min_Tc,
+        max_Tc=max_Tc,
         time_step=time_step,
         season_dict=season_dict,
         time_infect=time_infect,
@@ -51,6 +53,7 @@ def test_pandemic_runs():
         time_infect_units="year",
         gamma_shape=None,
         gamma_scale=None,
+        scenario_list=None,
     )
     assert (e[0] >= 0).all() and (e[0] <= 1).all()
     assert (e[1] >= 0).all() and (e[1] <= 1).all()
