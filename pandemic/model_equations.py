c4247739bdde184bb9dadc33df1aa5112b4252eb
arielsaffer <61440835+arielsaffer@users.noreply.github.com>
2021-11-04 11:39:16 -0400

    adapting intro binomial to annual; aggregate trade data and forecast to annual

--- pandemic/model_equations.py
+++ pandemic/model_equations.py
@@ -245,14 +245,7 @@ def pandemic_single_time_step(
                     chi_it,
                 )
             probability_of_establishment_ijt = probability_of_establishment(
-                alpha,
-                beta,
-                delta_kappa_ijt,
-                sigma_kappa,
-                h_jt,
-                sigma_h,
-                phi,
-                w_phi,
+                alpha, beta, delta_kappa_ijt, sigma_kappa, h_jt, sigma_h, phi, w_phi,
             )
         else:
             zeta_it = 0
@@ -267,7 +260,10 @@ def pandemic_single_time_step(
         introduction_probabilities[j, i] = probability_of_introduction_ijtc
 
         # decide if an introduction happens
-        introduced = np.random.binomial(1, probability_of_introduction_ijtc)
+        if len(time_step) == 4:
+            introduced = np.random.binomial(12, probability_of_introduction_ijtc)
+        else:
+            introduced = np.random.binomial(1, probability_of_introduction_ijtc)
         if bool(introduced):
             print("\t\t", origin["NAME"], "-->", destination["NAME"])
             introduction_country[j, i] = bool(introduced)
