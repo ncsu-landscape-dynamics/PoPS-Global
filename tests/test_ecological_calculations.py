72d2d33c63e5c7856dfdf78ddc5904a3b748aff9
Kellyn Montgomery <kellyn.montgomery@usda.gov>
2021-12-10 12:36:56 -0500

    Add test for climate_similarity_origins

--- tests/test_ecological_calculations.py
+++ tests/test_ecological_calculations.py
@@ -1,5 +1,8 @@
 import numpy as np
-from pandemic.ecological_calculations import climate_similarity
+import pandas as pd
+from pandemic.ecological_calculations import (
+    climate_similarity,
+    climate_similarity_origins)
 
 
 def test_climate_similarity():
@@ -7,3 +10,47 @@ def test_climate_similarity():
     destination_climates = np.array([0.25, 0, 0, 0.5, 0.25])
 
     assert climate_similarity(origin_climates, destination_climates) == 0.25
+
+
+def test_climate_similarity_origins():
+    climate_classes = [
+        "Af",
+        "Am",
+        "Aw",
+        "BWh",
+        "BWk",
+        "BSh",
+        "BSk",
+        "Csa",
+        "Csb",
+        "Csc",
+        "Cwa",
+        "Cwb",
+        "Cwc",
+        "Cfa",
+        "Cfb",
+        "Cfc",
+        "Dsa",
+        "Dsb",
+        "Dsc",
+        "Dsd",
+        "Dwa",
+        "Dwb",
+        "Dwc",
+        "Dwd",
+        "Dfa",
+        "Dfb",
+        "Dfc",
+        "Dfd",
+        "ET",
+        "EF",
+    ]
+    origins_climate_list = climate_classes[0:10]
+    destination_climates = pd.Series(
+        data=[1 / len(climate_classes)] * len(climate_classes), index=climate_classes
+    )
+
+    assert climate_similarity_origins(
+        origins_climate_list, destination_climates
+    ) == (1 / len(climate_classes)) * 10
+
